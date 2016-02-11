e9671564d9e6fecfec99e4d56026f8e81d767b8b
StackOverfl <josiah.hadley@gmail.com>
2016-02-10 17:37:49 -0800

    Initial Commit

---
+++ XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
@@ -0,0 +1,22 @@
+package com.example.xyzreader.remote;
+
+import android.util.Log;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Config {
+    public static final URL BASE_URL;
+    private static final String LOG_TAG = Config.class.getSimpleName();
+
+    static {
+        URL url = null;
+        try {
+            url = new URL("https://dl.dropboxusercontent.com/u/231329/xyzreader_data/data.json" );
+        } catch (MalformedURLException e) {
+            Log.e(LOG_TAG, "MalformedURLException caught: " + e.toString());
+        }
+
+        BASE_URL = url;
+    }
+}
